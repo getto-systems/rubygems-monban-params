0e0a9593cb771c8c0953960508868f8a45ae5200
shun <shun@getto.systems>
2018-11-13 08:16:16 +0000

    fix: push_tags

--- bin/push_tags.sh
+++ bin/push_tags.sh
@@ -4,4 +4,4 @@ git remote add super https://gett-systems:$GITLAB_ACCESS_TOKEN@gitlab.com/getto-
 git remote add github https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/rubygems-getto-params.git
 git tag $(cat .release-version)
 git push super HEAD:master --tags
-git push bitbucket HEAD:master --tags
+git push github HEAD:master --tags
